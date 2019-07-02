8f08f5384115231186bc0841ae20b4fe284594db
Kowit C <hi@kowito.com>
2019-07-02 22:16:35 +0700

    change debug variables

--- scarylog/settings.py
+++ scarylog/settings.py
@@ -23,13 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 INTERNAL_IPS = [
-    # ...
     '127.0.0.1',
-    # ...
 ]
 
 # Application definition
