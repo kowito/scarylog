0cae1255192118bd0f00ad3508c1b5801ea5d674
Kowit C <hi@kowito.com>
2019-07-02 22:18:23 +0700

    cast bool var

--- scarylog/settings.py
+++ scarylog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = eval(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = []
 INTERNAL_IPS = [
