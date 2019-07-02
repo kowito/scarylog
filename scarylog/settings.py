cc31efe5a26d76e93007466f1093d7580659c262
Kowit C <hi@kowito.com>
2019-07-02 21:03:27 +0700

    use s3 on prd only

--- scarylog/settings.py
+++ scarylog/settings.py
@@ -137,14 +137,18 @@ STATICFILES_DIRS = [
 
 ]
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = 'assets.scaylog.com'
 AWS_S3_SECURE_URLS = True
 AWS_DEFAULT_ACL = 'private'
+
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_CUSTOM_DOMAIN = 'assets.scaylog.com'
+
 
 LOCATION_FIELD_PATH = STATIC_URL + 'location_field'
 LOCATION_FIELD = {
