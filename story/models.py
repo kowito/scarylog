86b340151387447d15d74358babafb9b9e3e5364
Kowit C <hi@kowito.com>
2019-07-04 16:09:02 +0700

    change media file in frontend

--- story/models.py
+++ story/models.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext as _
+
 from django_extensions.db.fields import AutoSlugField
 from location_field.models.plain import PlainLocationField
 from ckeditor.fields import RichTextField
@@ -9,10 +11,10 @@ from ckeditor.fields import RichTextField
 class Story(models.Model):
 
     # Fields
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name=_("Story caption"))
     slug = AutoSlugField(populate_from='name', blank=True)
-    coordinate = PlainLocationField(based_fields=['name'], zoom=12)
-    description = RichTextField()
+    coordinate = PlainLocationField(based_fields=['name'], zoom=12, verbose_name=_("Location"))
+    description = RichTextField(verbose_name=_("Story"))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -24,6 +26,8 @@ class Story(models.Model):
 
     class Meta:
         ordering = ('-pk',)
+        verbose_name = _("story")
+        verbose_name_plural = _("stories")
 
     def __str__(self):
         return self.name
