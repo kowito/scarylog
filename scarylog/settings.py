ddea297406e45c3ba215ccc8a5c157d0aa508831
Kowit C <hi@kowito.com>
2019-07-04 19:43:26 +0700

    location base path debug

--- scarylog/settings.py
+++ scarylog/settings.py
@@ -143,10 +143,19 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+ROLLBAR = {
+    'access_token': os.getenv('ROLLBAR_TOKEN'),
+    'environment': 'development' if DEBUG else 'production',
+    'root': BASE_DIR,
+}
+
+
+import rollbar
+rollbar.init(**ROLLBAR)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets/'),
@@ -172,7 +181,7 @@ else:
     MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware', ] + MIDDLEWARE
     LOCATION_FIELD_PATH = STATIC_URL + 'location_field'
 
-
+rollbar.report_message('LOCATION_FIELD_PATH: {} '.format(LOCATION_FIELD_PATH))
 LOCATION_FIELD = {
     'map.provider': 'mapbox',
     'provider.mapbox.access_token': 'pk.eyJ1Ijoic2Nhcnlsb2ciLCJhIjoiY2p4b2xnOHRwMDg4MDNudXF2dnNoZ2w3NCJ9.ptdreoxFUFHQZAW2VQuzTw',
@@ -195,14 +204,3 @@ CKEDITOR_CONFIGS = {
         'toolbar': 'story',
     },
 }
-
-ROLLBAR = {
-    'access_token': os.getenv('ROLLBAR_TOKEN'),
-    'environment': 'development' if DEBUG else 'production',
-    'root': BASE_DIR,
-}
-
-
-import rollbar
-rollbar.init(**ROLLBAR)
-rollbar.report_message('LOCATION_FIELD_PATH: {} '.format(LOCATION_FIELD_PATH))
