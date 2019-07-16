3e125d2446aea95d906898cabe14414fc191c7cf
Kowit C <hi@kowito.com>
2019-07-16 23:07:58 +0700

    fix geo

--- story/models.py
+++ story/models.py
@@ -3,12 +3,13 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext as _
 from django.utils.html import strip_tags
-
 from django_extensions.db.fields import AutoSlugField
+
+
 from location_field.models.plain import PlainLocationField
 from ckeditor.fields import RichTextField
-from django.contrib.postgres.fields import JSONField
 import requests
+import json
 from scarylog.settings import GOOGLE_API_KEY
 
 
@@ -17,19 +18,19 @@ class Story(models.Model):
     # Fields
     name = models.CharField(max_length=255, verbose_name=_("Story caption"))
     slug = AutoSlugField(populate_from='name', blank=True)
-    __original_coordinate = None
-    coordinate = PlainLocationField(based_fields=['name'], zoom=12, verbose_name=_("Location"))
     description = RichTextField(verbose_name=_("Story"))
     short_desc = RichTextField(null=True, blank=True)
-    geocoding = JSONField(null=True, blank=True)
+    coordinate = PlainLocationField(based_fields=['name'], zoom=12, verbose_name=_("Location"))
+    geocoding = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # Relationship Fields
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE, related_name="stories",
     )
+
+    __original_coordinate = None
 
     class Meta:
         ordering = ('-pk',)
@@ -53,7 +54,6 @@ class Story(models.Model):
         if self.coordinate and self.__original_coordinate != self.coordinate:
             endpoint = f'https://maps.googleapis.com/maps/api/geocode/json?latlng={self.coordinate}&key={GOOGLE_API_KEY}'
             self.geocoding = requests.get(endpoint).json()
-
         super(Story, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
@@ -67,12 +67,12 @@ class Story(models.Model):
         return float(location[0]), float(location[1])
 
     def google_place_id(self):
-        if self.geocoding:
-            results = self.geocoding.get('results')
-            city = next(result for result in results if ('political' in result.get('types') and
-                                                         (
+        import ast
+        geo = ast.literal_eval(self.geocoding)
+        if geo:
+            results = geo.get('results')
+            city = next(result for result in results if ('political' in result.get('types') and (
                         'locality' in result.get('types') or
                         'administrative_area_level_1' in result.get('types')
-                        )
-            ))
+                        )))
             return city.get('place_id')
