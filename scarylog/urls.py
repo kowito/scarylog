9c0720e13dcc27b248b33770f9f54069b761997b
Kowit C <hi@kowito.com>
2019-07-18 21:46:19 +0700

    fix media path

--- scarylog/urls.py
+++ scarylog/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from story.views import StoryListView, map_view
 
 from scarylog import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
@@ -16,10 +16,9 @@ urlpatterns = [
     path('profile/', include('apps.profile.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
